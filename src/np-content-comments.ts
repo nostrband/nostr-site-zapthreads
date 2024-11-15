37c4683a27e471ff38e02130f47b318f26412a76
artur <brugeman.artur@gmail.com>
2024-11-15 13:42:18 +0100

    Minor fixes, better integration with nostrSite

--- src/np-content-comments.ts
+++ src/np-content-comments.ts
@@ -1,25 +1,37 @@
-import { html, LitElement } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { html, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { registerPlugin } from "./utils";
 
-@customElement('np-content-comments')
+@customElement("np-content-comments")
 export class Comments extends LitElement {
-	@property({ attribute: 'data-id' }) id =
-		'note1amhn0tq4c8efhjtescrm80qnnnsulagz809h0czv5jmr5245hxrq2zm6kw'
-	@property({ attribute: 'data-relays' }) relays = ''
-	@property({ attribute: 'data-client' }) client = ''
-	@property({ attribute: 'data-user' }) user = ''
+  @property({ attribute: "data-id" }) id =
+    "note1amhn0tq4c8efhjtescrm80qnnnsulagz809h0czv5jmr5245hxrq2zm6kw";
+  @property({ attribute: "data-relays" }) relays = "";
+  @property({ attribute: "data-client" }) client = "";
+  @property({ attribute: "data-user" }) user = "";
 
-	render() {
-		return html`
-			<zap-threads
-				.npubpro=${true}
-				.mode=${'chat'}
-				.disable=${'likes,zaps'}
-				.anchor="${this.id}"
-				.relays="${this.relays}"
-				.client="${this.client}"
-				.user=${this.user}
-			></zap-threads>
-		`
-	}
+  connectedCallback() {
+    super.connectedCallback();
+
+    registerPlugin().then((ep) => {
+      ep.subscribe("auth", (info: { type: string; pubkey: string }) => {
+        console.log("comments auth", info);
+        this.user = info.pubkey;
+      });
+    });
+  }
+
+  render() {
+    return html`
+      <zap-threads
+        npubpro=${"true"}
+        mode=${"chat"}
+        disable=${"likes,zaps"}
+        anchor="${this.id}"
+        relays="${this.relays}"
+        client="${this.client}"
+        user=${this.user}
+      ></zap-threads>
+    `;
+  }
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,31 @@
-export function getRecipientPubkey(npub: string = '') {
-	try {
-		// @ts-ignore
-		const { type, data } = window.nostrSite.nostrTools.nip19.decode(npub)
-		switch (type) {
-			case 'npub':
-				return data
-		}
-	} catch (e) {
-		console.log('bad author', npub, e)
-	}
-	return ''
+export function getRecipientPubkey(npub: string = "") {
+  try {
+    // @ts-ignore
+    const { type, data } = window.nostrSite.nostrTools.nip19.decode(npub);
+    switch (type) {
+      case "npub":
+        return data;
+    }
+  } catch (e) {
+    console.log("bad author", npub, e);
+  }
+  return "";
 }
+
+let pluginPromise: Promise<any> | undefined;
+
+export async function registerPlugin() {
+	return pluginPromise;
+}
+
+async function startRegisterPlugin() {
+  pluginPromise = new Promise(async (ok) => {
+    // @ts-ignore
+    if (!window.nostrSite)
+      await new Promise<Event>((ok) => document.addEventListener("npLoad", ok));
+    // @ts-ignore
+    ok(window.nostrSite.plugins.register("zapthreads"));
+  });
+}
+
+startRegisterPlugin();
